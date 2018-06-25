f24f9f8450a117edb53f5209d8c083b1f44a9abc
lumeng <lumeng@111.com.cn>
2018-06-25 11:07:56 +0800

    dwad

--- src/main/java/com/example/demo/ZuuldemoApplication.java
+++ src/main/java/com/example/demo/ZuuldemoApplication.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
+import com.example.demo.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -13,5 +15,10 @@ public class ZuuldemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuuldemoApplication.class, args);
+	}
+
+	@Bean
+	public AccessFilter accessFilter() {
+		return new AccessFilter();
 	}
 }
